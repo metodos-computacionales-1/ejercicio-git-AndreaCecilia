e9a093b94875f3350459258174c2e2ac0143347a
AndreaCecilia <ac.moreno432@uniandes.edu.co>
2019-10-31 03:09:44 +0000

    Se arreglo el codigo del ejercicio2 en main

--- 2.cpp
+++ 2.cpp
@@ -2,7 +2,7 @@
 int a;
 int b;
 int c;
-int main( a, b,  c){
+int main( argc = 3; argv[0] = “a”;argv[1] = “b”;argv[2] = “c”;){
     
     if((a*a+b*b=c*c)|(b*b+c*c=a*a)|(a*a+c*c=b*b)){
         std::cout << "Si puede " <<  std::endl;
