500001a674cdd83d308b5840113fa2216c41269b
AndreaCecilia <ac.moreno432@uniandes.edu.co>
2019-10-31 02:38:16 +0000

    se arreglo la funcion

--- 1.cpp
+++ 1.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <cmath>
 
+std::cout<< "Escribe el radio: ";
+std::cin >> int R;
 int main(){
   int i;
-  
-  for (i=0;i<20;i++){
-    std::cout << i << " " <<exp(i) << std::endl;
-  }
+  int pi=3.145;
+    float dia=R*2;
+    std::cout<< "diametro: ";
+    std::cout<< dia;
+    float peri=2*pi*R; 
+    std::cout<< "Perimetro";
+    std::cout<< peri;
+    float area=pi*R*R;
+    std::cout<< "Area";
+    std::cout<< area;
   
   return 0;
 }
